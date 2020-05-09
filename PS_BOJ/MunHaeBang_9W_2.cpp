578d75f15ffe2fc715fa9c6941e886d09e026ea0
wenserver3315 <webserver3315@naver.com>
2020-05-10 03:12:43 +0900

    문해방9주차

--- PS_BOJ/MunHaeBang_9W_2.cpp
+++ PS_BOJ/MunHaeBang_9W_2.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int n;
 vector<string> vct;
+int indegree[25002];
 vector<int> adj[25002];
+
+//bool connect(const string& a, const string& b) {
+bool connect(string a, string b) {
+	if (a.size() > b.size()) swap(a, b);
+	int asz = a.size(), bsz = b.size();
+	if (bsz - asz > 1) return 0;
+
+	int le = 0, ri = bsz - 1;
+	while (le < asz && a[le] == b[le]) le++;
+	if (asz == bsz) while (ri >= 0 && a[ri] == b[ri]) ri--;
+	else while (ri - 1 >= 0 && a[ri - 1] == b[ri]) ri--;
+	return le == ri;
+}
 
 bool isKun(const string& a, const string& b) {//0 또는 1 최단편집거리면 true, 아니면 false
 	int cnt = 0;//1번까지는 ㄱㄴ
@@ -42,6 +56,15 @@ bool isKun(const string& a, const string& b) {//0 또는 1 최단편집거리면 true, 아
 	}
 }
 
+int dfs(int cur, int d) {
+	int maxjmp = 1;
+	for (int next : adj[cur]) {
+		int nowjmp = dfs(next, d + 1);
+		if (maxjmp < nowjmp) maxjmp = nowjmp;
+	}
+	return maxjmp;
+}
+
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -51,13 +74,22 @@ signed main() {
 		string tmp;
 		cin >> tmp;
 		vct.push_back(tmp);
-		for (int j = 0; j < i; j++) {
+		for (int j = 0; j < i; j++) {//연결리스트 제조
 			if (isKun(vct[i], vct[j])) {
+				adj[j].push_back(i); indegree[i]++;
+				cout << " 내꺼 함수: " << vct[j] << " 와 " << vct[i] << " 는 편집거리 0 또는 1" << endl;
+			}
+			/*if (connect(vct[i], vct[j])) {
 				adj[j].push_back(i);
-				//cout << vct[j] << " 와 " << vct[i] << " 는 편집거리 0 또는 1" << endl;
+				cout << " ㅈㄱ 함수: " << vct[j] << " 와 " << vct[i] << " 는 편집거리 0 또는 1" << endl;
+			}*/
+		}
+		for (int i = 0; i < n; i++) {
+			if (indegree[i]) continue;
+			else {
+				cout<<dfs
 			}
 		}
-
 
 
 	}
